d8cf625fe5ded3a940305f3782eec4af91c5b686
yuwki0131 <yuwkixxx@gmail.com>
2024-11-17 02:56:18 +0900

    add sample controller of backend

---
+++ backend/src/main/java/jp/readscape/backend/controller/SampleController.java
@@ -0,0 +1,15 @@
+package jp.readscape.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class SampleController {
+
+    @GetMapping
+    public String getSampleMessage() {
+        return "Hello, Readscape!";
+    }
+}

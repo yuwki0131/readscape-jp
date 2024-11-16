bb13bdf0c09580fb518179c9f61b966c634a2255
yuwki0131 <yuwkixxx@gmail.com>
2024-11-17 02:12:12 +0900

    init backend

---
+++ backend/src/main/java/jp/readscape/backend/ReadscapeBackendApplication.java
@@ -0,0 +1,12 @@
+package jp.readscape.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadscapeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadscapeBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/jp/readscape/backend/ReadscapeBackendApplicationTests.java
@@ -0,0 +1,8 @@
+package jp.readscape.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadscapeBackendApplicationTests {
+}
